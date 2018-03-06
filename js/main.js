4b2bac9f6af8ed844d879495a3cdc163a9d263eb
Zhang Yixiao <ldzhangyx@outlook.com>
2018-03-06 14:12:50 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
+      //$('.content-wrapper').addClass('animated slideInRight')
     } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
+      //$('.panel-cover').css('max-width', currentWidth)
+      //$('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
     }
   })
 
